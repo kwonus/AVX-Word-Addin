0b201657ae0277adb6b9aef47ee24cfae4762078
Kevin Wonus <kevin@wonus.com>
2025-01-04 19:25:56 -0500

    Rev the language rtf for digital-av 5.1 release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.24.12.27")]
-[assembly: AssemblyFileVersion("9.24.12.27")]
+[assembly: AssemblyVersion("9.25.1.04")]
+[assembly: AssemblyFileVersion("9.25.1.04")]
 
